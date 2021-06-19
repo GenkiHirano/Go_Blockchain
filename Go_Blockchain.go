6abc0edd5e8b91bbc6be0aee09babc0506e949c8
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-06-19 14:31:12 +0900

    ブロックの作成

--- Go_Blockchain.go
+++ Go_Blockchain.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Println("timestamp         %d/n, b.timestamp")
+	fmt.Println("nonce             %d/n, b.nonce")
+	fmt.Println("previousHash      %s/n, b.previousHash")
+	fmt.Println("transactions      %s/n, b.transactions")
+}
 
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	log.Println("test")
-	fmt.Println("test2")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
